7435ea6cd8a539fecf7d532773b2b0b3307c9e85
chnam83 <127906363+chnam83@users.noreply.github.com>
2023-10-12 09:04:13 -0700

    aider: Added a form with an input field and a submit button to the App component.
    # Aider chat conversation:
    
    ASSISTANT: Great! Now that we have access to `src/App.js`, we can start setting up a basic User Interface.
    
    Let's create a simple form with an input field and a submit button.
    
    Here are the changes we need to make:
    
    1. Import the `useState` hook from React at the top of the file.
    2. Inside the `App` function, declare a new state variable `inputValue` and its setter function `setInputValue` using the `useState` hook. Initialize `inputValue` with an empty string.
    3. Create a `handleInputChange` function that updates `inputValue` with the current value of the input field every time the user types into it.
    4. Create a `handleSubmit` function that prevents the page from refreshing when the form is submitted and logs `inputValue` to the console.
    5. Replace the `Hello, world!` message with a form that includes an input field and a submit button. The input field should use `inputValue` as its value and `handleInputChange` as its `onChange` handler. The form should use `handleSubmit` as its `onSubmit` handler.
    
    Here's the *edit block* for these changes:
    
    ```javascript
    src/App.js
    <<<<<<< HEAD
    import React from 'react';
    
    function App() {
      return (
        <div className="App">
          <h1>Hello, world!</h1>
        </div>
      );
    }
    
    export default App;
    =======
    import React, { useState } from 'react';
    
    function App() {
      const [inputValue, setInputValue] = useState('');
    
      const handleInputChange = (event) => {
        setInputValue(event.target.value);
      };
    
      const handleSubmit = (event) => {
        event.preventDefault();
        console.log(inputValue);
      };
    
      return (
        <div className="App">
          <form onSubmit={handleSubmit}>
            <input type="text" value={inputValue} onChange={handleInputChange} />
            <button type="submit">Submit</button>
          </form>
        </div>
      );
    }
    
    export default App;
    >>>>>>> updated
    ```

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+  };
+
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
