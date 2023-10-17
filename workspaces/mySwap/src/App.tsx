a375f4b7f6ba663ff72df6100a6eb3c45d5b48fd
chnam83 <127906363+chnam83@users.noreply.github.com>
2023-10-17 12:32:41 -0700

    aider: Changed import in src/index.js to import App from '../workspaces/mySwap/src/App' and added useState and axios to workspaces/mySwap/src/App.tsx.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: workspaces/mySwap/src/App.tsx
    
    ASSISTANT: Great! Now that we have the `src/index.js` and `workspaces/mySwap/src/App.tsx` files as *read-write*, we can proceed with the modifications.
    
    First, we need to change the `src/index.js` file to import the `App` component from the `workspaces/mySwap/src/App.tsx` file instead of the `src/App.js` file.
    
    Here is the *edit block* for the `src/index.js` file:
    
    ```javascript
    src/index.js
    <<<<<<< HEAD
    import App from './App';
    =======
    import App from '../workspaces/mySwap/src/App';
    >>>>>>> updated
    ```
    
    Next, we need to change the `workspaces/mySwap/src/App.tsx` file to use the `useState` hook and `axios` for HTTP requests, similar to the `src/App.js` file.
    
    Here is the *edit block* for the `workspaces/mySwap/src/App.tsx` file:
    
    ```typescript
    workspaces/mySwap/src/App.tsx
    <<<<<<< HEAD
    import React from 'react';
    
    const App: React.FC = () => {
      return (
        <div className="App">
          <header>
            <h1>My App</h1>
          </header>
          <main>
            <p>Welcome to my app!</p>
          </main>
          <footer>
            <p>© 2023 My App</p>
          </footer>
        </div>
      );
    };
    
    export default App;
    =======
    import React, { useState, FC } from 'react';
    import axios from 'axios';
    
    const App: FC = () => {
      const [inputValue, setInputValue] = useState('');
    
      const handleInputChange = (event) => {
        setInputValue(event.target.value);
      };
    
      const handleSubmit = async (event) => {
        event.preventDefault();
        try {
          const response = await axios.get(`https://blockchain.info/rawaddr/${inputValue}`);
          console.log(response.data);
        } catch (error) {
          console.error(error);
        }
      };
    
      return (
        <div className="App">
          <form onSubmit={handleSubmit}>
            <input type="text" value={inputValue} onChange={handleInputChange} />
            <button type="submit">Submit</button>
          </form>
        </div>
      );
    }
    
    export default App;
    >>>>>>> updated
    ```
    
    Please apply these changes and let me know if you have any questions.

--- workspaces/mySwap/src/App.tsx
+++ workspaces/mySwap/src/App.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState, FC } from 'react';
+import axios from 'axios';
 
-const App: React.FC = () => {
+const App: FC = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.get(`https://blockchain.info/rawaddr/${inputValue}`);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header>
-        <h1>My App</h1>
-      </header>
-      <main>
-        <p>Welcome to my app!</p>
-      </main>
-      <footer>
-        <p>© 2023 My App</p>
-      </footer>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
-};
+}
 
 export default App;
