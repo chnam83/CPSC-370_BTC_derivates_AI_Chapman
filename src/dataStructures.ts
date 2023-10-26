2ed8982e33ae850457d7eaf556cec70db650e341
chnam83 <127906363+chnam83@users.noreply.github.com>
2023-10-26 16:23:55 -0700

    aider: Implemented placeOrder, cancelOrder, and matchOrders for CLOB in src/dataStructures.ts.
    # Aider chat conversation:
    
    USER: I added these *read-write* files: src/dataStructures.ts
    
    ASSISTANT: Great! Now that we have the `src/dataStructures.ts` file in read-write mode, we can make changes to it. Could you please specify what changes you would like to make to this file?
    
    USER: implement placeOrder, cancelOrder, and matchOrders for CLOB
    
    ASSISTANT: To implement `placeOrder`, `cancelOrder`, and `matchOrders` for CLOB, we need to modify the `src/dataStructures.ts` file.
    
    Here are the steps we need to follow:
    
    1. Define the `placeOrder` function. This function will take an `Order` and an `OrderBook` as parameters. It will add the order to the order book and return the updated order book.
    
    2. Define the `cancelOrder` function. This function will take an `Order` and an `OrderBook` as parameters. It will remove the order from the order book and return the updated order book.
    
    3. Define the `matchOrders` function. This function will take an `OrderBook` as a parameter. It will match the buy and sell orders based on the price and quantity and return the matched orders.
    
    Here are the proposed changes:
    
    ```typescript
    src/dataStructures.ts
    <<<<<<< HEAD
    type Order = {
      id: string;
      type: 'BUY' | 'SELL';
      price: number;
      quantity: number;
    };
    
    type OrderBook = Order[];
    =======
    type Order = {
      id: string;
      type: 'BUY' | 'SELL';
      price: number;
      quantity: number;
    };
    
    type OrderBook = Order[];
    
    function placeOrder(order: Order, orderBook: OrderBook): OrderBook {
      return [...orderBook, order];
    }
    
    function cancelOrder(order: Order, orderBook: OrderBook): OrderBook {
      return orderBook.filter(o => o.id !== order.id);
    }
    
    function matchOrders(orderBook: OrderBook): OrderBook {
      // This is a simple implementation. In a real-world application, you would need a more complex algorithm to match orders.
      const buyOrders = orderBook.filter(o => o.type === 'BUY').sort((a, b) => b.price - a.price);
      const sellOrders = orderBook.filter(o => o.type === 'SELL').sort((a, b) => a.price - b.price);
      const matchedOrders = [];
    
      while (buyOrders.length > 0 && sellOrders.length > 0) {
        const buyOrder = buyOrders[0];
        const sellOrder = sellOrders[0];
    
        if (buyOrder.price >= sellOrder.price) {
          const quantity = Math.min(buyOrder.quantity, sellOrder.quantity);
          matchedOrders.push({ ...buyOrder, quantity }, { ...sellOrder, quantity });
    
          if (buyOrder.quantity > sellOrder.quantity) {
            buyOrder.quantity -= sellOrder.quantity;
            sellOrders.shift();
          } else if (buyOrder.quantity < sellOrder.quantity) {
            sellOrder.quantity -= buyOrder.quantity;
            buyOrders.shift();
          } else {
            buyOrders.shift();
            sellOrders.shift();
          }
        } else {
          break;
        }
      }
    
      return matchedOrders;
    }
    >>>>>>> updated
    ```
    
    Please note that the `matchOrders` function is a simple implementation. In a real-world application, you would need a more complex algorithm to match orders.

--- src/dataStructures.ts
+++ src/dataStructures.ts
@@ -6,3 +6,43 @@ type Order = {
 };
 
 type OrderBook = Order[];
+
+function placeOrder(order: Order, orderBook: OrderBook): OrderBook {
+  return [...orderBook, order];
+}
+
+function cancelOrder(order: Order, orderBook: OrderBook): OrderBook {
+  return orderBook.filter(o => o.id !== order.id);
+}
+
+function matchOrders(orderBook: OrderBook): OrderBook {
+  // This is a simple implementation. In a real-world application, you would need a more complex algorithm to match orders.
+  const buyOrders = orderBook.filter(o => o.type === 'BUY').sort((a, b) => b.price - a.price);
+  const sellOrders = orderBook.filter(o => o.type === 'SELL').sort((a, b) => a.price - b.price);
+  const matchedOrders = [];
+
+  while (buyOrders.length > 0 && sellOrders.length > 0) {
+    const buyOrder = buyOrders[0];
+    const sellOrder = sellOrders[0];
+
+    if (buyOrder.price >= sellOrder.price) {
+      const quantity = Math.min(buyOrder.quantity, sellOrder.quantity);
+      matchedOrders.push({ ...buyOrder, quantity }, { ...sellOrder, quantity });
+
+      if (buyOrder.quantity > sellOrder.quantity) {
+        buyOrder.quantity -= sellOrder.quantity;
+        sellOrders.shift();
+      } else if (buyOrder.quantity < sellOrder.quantity) {
+        sellOrder.quantity -= buyOrder.quantity;
+        buyOrders.shift();
+      } else {
+        buyOrders.shift();
+        sellOrders.shift();
+      }
+    } else {
+      break;
+    }
+  }
+
+  return matchedOrders;
+}
